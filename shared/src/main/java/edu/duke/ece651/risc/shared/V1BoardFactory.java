8e80663b084efeceda8619839d593d9888b3f828
Denise Liu <xl340@duke.edu>
2021-03-14 16:45:56 +0800

    set location map

--- shared/src/main/java/edu/duke/ece651/risc/shared/V1BoardFactory.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/V1BoardFactory.java
@@ -7,7 +7,21 @@ public class V1BoardFactory<T> implements BoardFactory<T>{
     initTerriName();
   }
   protected void initTerriName(){
-    terriName.put(0,"");
+    terriName.put(0,"Narnia");
+    terriName.put(1,"Midkemia");
+    terriName.put(2,"Oz");
+    terriName.put(3,"Elantris");
+    terriName.put(4,"Roshar");
+    terriName.put(5,"Scadrial");
+    terriName.put(6,"Gondor");
+    terriName.put(7,"Mordor");
+    terriName.put(8,"Hogwarts");
+    terriName.put(9,"Narnia");
+    terriName.put(10,"Britt");
+    terriName.put(12,"Calibre");
+    terriName.put(13,"Elk");
+    terriName.put(14,"Floyd");
+    
   }
   protected Board<T> make2PlayerBoard(){
     Board<T> b = new V1GameBoard<T>();
