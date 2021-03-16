1d2ce9b7ce6bb80befd3f9724c1a92e5ebcf2448
yidansun0112 <ys303@duke.edu>
2021-03-16 12:04:35 +0800

    finish writing and testing deployUnits

--- client/src/test/java/edu/duke/ece651/risc/client/V1GamePlayerTest.java
+++ client/src/test/java/edu/duke/ece651/risc/client/V1GamePlayerTest.java
@@ -37,13 +37,11 @@ public class V1GamePlayerTest {
   public void test_selectPlayerNum() throws IOException{
     MockitoAnnotations.initMocks(this);
     ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-    V1GamePlayer<String> player=createV1GamePlayer("11\na\n1\n6\n4\n", bytes);
-    // TODO: player doesn't need to know the type of exception, only know why his input is wrong would be enough
+    V1GamePlayer<String> player=createV1GamePlayer("a\n1\n6\n4\n", bytes);
     String prompt="You are the first player in this round, please choose how many players you want in this round.\nPlayer number should be from 2 to 5.\nPlease make your choice:\n";
-    String exceptLen="Exception thrown:java.lang.IllegalArgumentException: "+"Player number should only be one digit.\n"+"Please do that again!\n";
-    String exceptDigit="Exception thrown:java.lang.IllegalArgumentException: "+"Player number should be digit.\n"+"Please do that again!\n";
-    String exceptRange="Exception thrown:java.lang.IllegalArgumentException: "+"Player number should be from 2 to 5.\n"+"Please do that again!\n";
-    String total=prompt+exceptLen+prompt+exceptDigit+prompt+exceptRange+prompt+exceptRange+prompt;
+    String exceptDigit="For input string: \"a\" Player number should be digits.\n"+"Please do that again!\n";
+    String exceptRange="Player number should be from 2 to 5.\n"+"Please do that again!\n";
+    String total=prompt+exceptDigit+exceptRange+exceptRange;
     player.selectPlayerNum();
     assertEquals(total,bytes.toString());
   }
@@ -52,16 +50,16 @@ public class V1GamePlayerTest {
   public void test_selectGameMap() throws IOException,ClassNotFoundException{
     MockitoAnnotations.initMocks(this);
     String mapChoices="Map 0: HarryPort\nMap 1: The Rings\n";
-    when(clientMock.receiveObject()).thenReturn(mapChoices,mapChoices,"Invalid choice",mapChoices,Constant.VALID_MAP_CHOICE_INFO);
+    when(clientMock.receiveObject()).thenReturn(mapChoices,"Invalid choice",Constant.VALID_MAP_CHOICE_INFO);
     ByteArrayOutputStream bytes = new ByteArrayOutputStream();
     V1GamePlayer<String> player=createV1GamePlayer("a\n2\n1\n", bytes);
     String p1="Please choose one map among the following maps.\n";
     String p2="Please type the map number that you would like to choose:\n";
     String prompt=p1+mapChoices+"\n"+p2;
-    String exceptNotNum="Exception thrown:java.lang.IllegalArgumentException: "+"Map number should be pure number.\n"+"Please do that again!\n";
-    String exceptInvalid="Exception thrown:java.lang.IllegalArgumentException: "+"Invalid choice\n"+"Please do that again!\n";
+    String exceptNotNum="For input string: \"a\" Map number should be pure number.\n"+"Please do that again!\n";
+    String exceptInvalid="Invalid choice\n"+"Please do that again!\n";
     String successInfo=Constant.VALID_MAP_CHOICE_INFO + "\n";
-    String total=prompt+exceptNotNum+prompt+exceptInvalid+prompt+successInfo;
+    String total=prompt+exceptNotNum+exceptInvalid+successInfo;
     player.selectGameMap();
     assertEquals(total,bytes.toString());
   }
@@ -91,17 +89,36 @@ public class V1GamePlayerTest {
   public void test_pickTerritory() throws IOException,ClassNotFoundException{
     MockitoAnnotations.initMocks(this);
     String groupChoices="Group 0: Hogwarts\nGroup 1: Oz\n";
-    when(clientMock.receiveObject()).thenReturn(groupChoices,groupChoices,"Invalid choice",groupChoices,Constant.VALID_MAP_CHOICE_INFO);
+    when(clientMock.receiveObject()).thenReturn(groupChoices,"Invalid choice",Constant.VALID_MAP_CHOICE_INFO);
     ByteArrayOutputStream bytes = new ByteArrayOutputStream();
     V1GamePlayer<String> player=createV1GamePlayer("a\n2\n1\n", bytes);
     String p1="Please choose one group among the following groups.\n";
     String p2="Please type the group number that you would like to choose:\n";
     String prompt=p1+groupChoices+"\n"+p2;
-    String exceptNotNum="Exception thrown:java.lang.IllegalArgumentException: "+"Group number should be pure number.\n"+"Please do that again!\n";
-    String exceptInvalid="Exception thrown:java.lang.IllegalArgumentException: "+"Invalid choice\n"+"Please do that again!\n";
+    String exceptNotNum="For input string: \"a\" Group number should be pure number.\n"+"Please do that again!\n";
+    String exceptInvalid="Invalid choice\n"+"Please do that again!\n";
     String successInfo=Constant.VALID_MAP_CHOICE_INFO + "\n";
-    String total=prompt+exceptNotNum+prompt+exceptInvalid+prompt+successInfo;
+    String total=prompt+exceptNotNum+exceptInvalid+successInfo;
     player.pickTerritory();
+    assertEquals(total,bytes.toString());
+  }
+
+  @Test
+  public void test_deployUnits() throws IOException,ClassNotFoundException{
+    MockitoAnnotations.initMocks(this);
+    String territoryInfo="Territory 0: Hogwarts\nTerritory 1: Oz\n";
+    when(clientMock.receiveObject()).thenReturn(territoryInfo,territoryInfo,Constant.FINISH_DEPLOY_INFO);
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    V1GamePlayer<String> player=createV1GamePlayer("1 5\n,5\n1,\na,5\n1,a\n0,5\n1,5\n", bytes);
+    String prompt="Please deploy units in your territories.\n";
+    String p1="Please type in format \"Territory index,Units deployed in this territory\"\n";
+    String p2="For example \"2,5\" will deploy 5 units in territory 2\n";
+    String doChoice=territoryInfo+"\n"+p1+p2;
+    String exceptComma="Cannot find \",\" in your deployment.\n"+"Please do that again!\n";
+    String exceptNotNumA="For input string: \"a\" Territory/Units should be pure number.\n"+"Please do that again!\n";
+    String exceptNotNumNull="For input string: \"\" Territory/Units should be pure number.\n"+"Please do that again!\n";
+    String total=prompt+doChoice+exceptComma+doChoice+exceptNotNumNull+doChoice+exceptNotNumNull+doChoice+exceptNotNumA+doChoice+exceptNotNumA+doChoice+doChoice+Constant.FINISH_DEPLOY_INFO+"\n";
+    player.deployUnits();
     assertEquals(total,bytes.toString());
   }
 }
