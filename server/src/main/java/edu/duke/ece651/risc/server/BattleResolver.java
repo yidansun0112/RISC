81c67ce2364f37d1aa3c287f6820344108acf236
yidansun0112 <ys303@duke.edu>
2021-03-17 11:05:03 +0800

    start to write resolver

---
+++ server/src/main/java/edu/duke/ece651/risc/server/BattleResolver.java
@@ -0,0 +1,14 @@
+package edu.duke.ece651.risc.server;
+
+import edu.duke.ece651.risc.shared.Board;
+
+public class BattleResolver<T> implements Resolver<T> {
+  
+  @Override
+  public void combineEnemyArmy(Board<T> board){
+    //go through every territory
+    //go through every enemyArmy
+    //and combine
+    
+  }
+}
