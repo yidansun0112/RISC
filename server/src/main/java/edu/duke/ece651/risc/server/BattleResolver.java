3af3b48957222a4cad121b0bb809b29c4ebb995e
yidansun0112 <ys303@duke.edu>
2021-03-17 13:22:56 +0800

    edit battleresolver

--- server/src/main/java/edu/duke/ece651/risc/server/BattleResolver.java
+++ server/src/main/java/edu/duke/ece651/risc/server/BattleResolver.java
@@ -1,8 +1,10 @@
 package edu.duke.ece651.risc.server;
 
-import edu.duke.ece651.risc.shared.Board;
-import edu.duke.ece651.risc.shared.Territory;
+import edu.duke.ece651.risc.shared.*;
+
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Vector;
 
 public class BattleResolver<T> implements Resolver<T> {
   
@@ -17,26 +19,40 @@ public class BattleResolver<T> implements Resolver<T> {
     //go through every territory
     //go through every enemyArmy
     //and combine
-
+    
   }
 
   public void executeAllBattle(Board<T> board){
-    //combineEnemyArmy
-    //go through territory
-    //combatOnTerritory
+    //TODO : combineEnemyArmy
+    //go through territory and combatOnTerritory
+    ArrayList<Territory<T>> territories=board.getTerritories();
+    for(int i=0;i<territories.size();i++){
+      combatOnTerritory(territories.get(i));
+    }
   }
 
   public void combatOnTerritory(Territory<T> battleField){
-    //while loop untile enemyArmy is empty
-    //get an random index amony (0-enemyArmy size)
-    //then get currDefender and enemy, and pass it to combatBetween
-    //winner = conbatBetween return
-    //delete enemy
-    //currDefender=winner
+    Vector<Army<T>> currDefender=battleField.getCurrDefenderArmy();
+    Vector<Army<T>> enemyArmy=battleField.getEnemyArmy();
+    //while loop until enemyArmy is empty
+    while(enemyArmy.size()>0){
+      //get an random index amony (0-enemyArmy size)
+      int index=rdm.nextInt(enemyArmy.size());
+      Army<T> defender=currDefender.get(index);
+      Army<T> attacker=enemyArmy.get(index);
+      //winner = conbatBetween return
+      currDefender.set(0,combatBetween(defender,attacker));
+      //delete enemy
+      enemyArmy.remove(index);
+    }
+    //maybe need to reset currDefender and enemyArmy on this territory
+    //do not know whether origin value will be changed or not
   }
 
   /**
    * combat between two army.
+   * 
+   * @return winner
    */
   public Army<T> combatBetween(Army<T> defender, Army<T> attacker){
     //while loop (defender and attacker left both >0)
@@ -49,7 +65,7 @@ public class BattleResolver<T> implements Resolver<T> {
         attacker.minusUnit(1);
       }
       else{
-        defend.minusUnit(1);
+        defender.minusUnit(1);
       }
     }
     if(defender.getUnits()>0){
