a3c226bfb94df26a7108fd6b0334ddd63dc0360e
zl246 <zl246@duke.edu>
2021-03-17 03:15:23 +0000

    Merge featureLEGO into featureTryIntegrate to adopt new changes

---
+++ shared/src/test/java/edu/duke/ece651/risc/shared/AttackOrderPathCheckerTest.java
@@ -0,0 +1,30 @@
+package edu.duke.ece651.risc.shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AttackOrderPathCheckerTest {
+  @Test
+  public void test_path() {
+    BoardFactory<String> f = new V1BoardFactory<>();
+    Board<String> b = f.makeGameBoard(2);
+    OrderRuleChecker<String> r = new AttackOrderPathChecker<String>(null);
+    b.occupyTerritory(0, 0);
+    b.occupyTerritory(1, 1);
+   
+    Order<String> o1 = new AttackOrder<String>("0 3 2");
+    assertEquals(r.checkOrder(0,o1,b),null);
+    
+    Order<String> o2 = new AttackOrder<String>("1 4 2");
+    assertEquals(r.checkOrder(0,o2,b),"invalid order: You can only attack an enemy territory that is next to you!\n");
+  }
+
+}
+
+
+
+
+
+
+
---
+++ shared/src/test/java/edu/duke/ece651/risc/shared/MoveOrderTest.java
@@ -0,0 +1,38 @@
+package edu.duke.ece651.risc.shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MoveOrderTest {
+  @Test
+  public void test_MoveOrder() {
+
+    String s1 = "0 1 2";
+    MoveOrder<String> m1 = new MoveOrder<>(s1);
+    assertEquals(0, m1.getSrcTerritory());
+    assertEquals(1, m1.getDestTerritory());
+    assertEquals(2, m1.getUnitAmount());
+
+    String s2 = "";
+    String s3 = "kasdjla";
+    String s4 = "12 *& ajdo";
+    assertThrows(IllegalArgumentException.class, () -> new MoveOrder<String>(s2));
+    assertThrows(IllegalArgumentException.class, () -> new MoveOrder<String>(s3));
+    assertThrows(IllegalArgumentException.class, () -> new MoveOrder<String>(s4));
+
+    // Create the Board
+    BoardFactory<String> f = new V1BoardFactory<>();
+    Board<String> b = f.makeGameBoard(2);
+    b.occupyTerritory(0, 0);
+    b.occupyTerritory(1, 1);
+
+    MoveOrder<String> m2 = new MoveOrder<>("7 7 7");
+    assertEquals(false, m2.execute(b));
+    assertEquals(0, b.getTerritories().get(m1.getSrcTerritory()).getUnitAmount());
+    assertEquals(true, m1.execute(b));
+
+    assertThrows(IllegalArgumentException.class, () -> new MoveOrder<>("0 1 -1"));
+  }
+
+}
