141307f70ded0335d6a49065bdaf13ef40e4f198
zl246 <zl246@duke.edu>
2021-03-19 20:40:02 -0400

    <docs> add javadoc

--- server/src/main/java/edu/duke/ece651/risc/server/GameServer.java
+++ server/src/main/java/edu/duke/ece651/risc/server/GameServer.java
@@ -1,5 +1,8 @@
 package edu.duke.ece651.risc.server;
 
+/**
+ * A interface which mainly for refactoring. Remain this will let the code
+ * relatively more convienient */
+ 
 public interface GameServer<T> {
-  // TODO: decide what methods can put here!
 }
