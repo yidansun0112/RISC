329842ac75e2050ff87cb79f603b160a53705c1b
zl246 <zl246@duke.edu>
2021-03-25 23:34:45 -0400

    <feat> create new class Unit

---
+++ shared/src/main/java/edu/duke/ece651/risc/shared/Unit.java
@@ -0,0 +1,7 @@
+package edu.duke.ece651.risc.shared;
+
+/**
+ * This is a class used in evo2 to represent a unit instance. Since currently
+ * this class is an "empty" class, it does not have a unit test.
+ */
+public class Unit {}
