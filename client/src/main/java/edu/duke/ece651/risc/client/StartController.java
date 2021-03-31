7b4b1d17a07ab51486b855fe6ca2c10377507d04
Denise Liu <xl340@duke.edu>
2021-03-31 22:12:28 +0800

    do the select number

--- client/src/main/java/edu/duke/ece651/risc/client/StartController.java
+++ client/src/main/java/edu/duke/ece651/risc/client/StartController.java
@@ -1,26 +1,53 @@
 package edu.duke.ece651.risc.client;
 
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
-import javafx.util.Pair;
-
-import java.io.IOException;
 
 public class StartController {
-  @FXML private Button Start;
+  @FXML
+  private Button Start;
+  @FXML
+  private ChoiceBox<Integer> PlayerNumBox;
+  @FXML
+  private Button ConfirmNumBtn;
   private Stage Window;
-  public StartController( Stage Window){
-        this.Window = Window;
-        System.out.println("[DEBUG] Inside Start Controller Constructor");
+  SocketClient client;
+
+  public StartController(Stage Window) {
+    this.Window = Window;
+    PlayerNumBox = new ChoiceBox<>();
+    System.out.println("[DEBUG] Inside Start Controller Constructor");
   }
 
   @FXML
-  public void startGame() throws IOException, ClassNotFoundException{
-    SocketClient client=new SocketClient(12345,"127.0.0.1");
-    int playerId=recvID(client);
+  public void initialize() {
+    PlayerNumBox.setValue(2);
+    ObservableList<Integer> ChoiceNumber = FXCollections.observableArrayList(2, 3, 4, 5);
+    PlayerNumBox.setItems(ChoiceNumber);
+  }
+
+  @FXML
+  public void startGame() throws IOException, ClassNotFoundException {
+    client = new SocketClient(12345, "127.0.0.1");
+    int playerId = recvID();
+    if (playerId == 0) {
+      FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/ui/choosePlayerNum.fxml"));
+      loaderStart.setControllerFactory(c -> {
+        return new StartController(Window);
+      });
+      Scene scene = new Scene(loaderStart.load());
+
+      Window.setScene(scene);
+      Window.show();
+    }
   }
 
   /**
@@ -28,18 +55,23 @@ public class StartController {
    * receiving it, and print the ID to the command line. (This method is called by
    * joinGame() and the player ID will be changed at that time.)
    */
-  public int recvID(SocketClient client) throws ClassNotFoundException, IOException {
+  public int recvID() throws ClassNotFoundException, IOException {
     String strID = (String) client.receiveObject();
     int playerId = Integer.parseInt(strID);
     return playerId;
   }
 
+  @FXML
+  public void selectPlayerNum() throws ClassNotFoundException, IOException {
+    int num = PlayerNumBox.getValue();
+    client.sendObject(num);
+    FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/ui/choosePlayerNum.fxml"));
+    loaderStart.setControllerFactory(c -> {
+      return new StartController(Window);
+    });
+    Scene scene = new Scene(loaderStart.load());
+
+    Window.setScene(scene);
+    Window.show();
+  }
 }
-
-
-
-
-
-
-
-
