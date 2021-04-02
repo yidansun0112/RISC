92ed8ce2d24d834a187c7c3864fba509649788af
yidansun0112 <ys303@duke.edu>
2021-04-02 20:54:45 +0800

    issueorder, attack and move finish

--- client/src/main/java/edu/duke/ece651/risc/client/IssueOrderController.java
+++ client/src/main/java/edu/duke/ece651/risc/client/IssueOrderController.java
@@ -13,25 +13,25 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class DeployUnitsController implements Initializable{
+public class IssueOrderController implements Initializable{
   @FXML
   AnchorPane mapPane;
   @FXML
   AnchorPane rootPane;
   @FXML
-  private Button confirmBtn;
+  private Button moveBtn;
   @FXML
-  private ChoiceBox<String> terrBox;
+  private Button attackBtn;
   @FXML
-  private ChoiceBox<String> amountBox;
+  private Button upgradeBtn;
+  @FXML
+  private Button doneBtn;
   private Stage window;
   private GUIPlayer player;
 
-  public DeployUnitsController(Stage window,GUIPlayer player) {
+  public IssueOrderController(Stage window,GUIPlayer player) {
     this.window = window;
     this.player=player;
-    terrBox=new ChoiceBox<>();
-    amountBox=new ChoiceBox<>();
     FXMLLoader mapLoader = new FXMLLoader(getClass().getResource("/ui/map2link.fxml"));
     mapLoader.setControllerFactory(c -> {
       if(c.equals(MapLinkController.class)){
@@ -50,13 +50,22 @@ public class DeployUnitsController implements Initializable{
     }
   }
 
+
+  public void initialize(URL url, ResourceBundle rb){
+    rootPane.getChildren().add(mapPane);
+    AnchorPane.setTopAnchor(mapPane,0.0);
+    AnchorPane.setRightAnchor(mapPane,0.0);
+    AnchorPane.setLeftAnchor(mapPane,0.0);
+    AnchorPane.setBottomAnchor(mapPane,200.0);
+  } 
+
   @FXML
-  public void deploy(){
+  public void move(){
     try{
-      FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/ui/issueOrder.fxml"));
+      FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/ui/moveAttack.fxml"));
       loaderStart.setControllerFactory(c -> {
-        if(c.equals(IssueOrderController.class)){
-          return new IssueOrderController(window,player);
+        if(c.equals(MoveAttackController.class)){
+          return new MoveAttackController(window,player);
         }
         try{
           return c.getConstructor().newInstance();
@@ -72,11 +81,35 @@ public class DeployUnitsController implements Initializable{
   }
   }
 
-  public void initialize(URL url, ResourceBundle rb){
-    rootPane.getChildren().add(mapPane);
-    AnchorPane.setTopAnchor(mapPane,0.0);
-    AnchorPane.setRightAnchor(mapPane,0.0);
-    AnchorPane.setLeftAnchor(mapPane,0.0);
-    AnchorPane.setBottomAnchor(mapPane,200.0);
-  } 
+  @FXML
+  public void attack(){
+    try{
+      FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/ui/moveAttack.fxml"));
+      loaderStart.setControllerFactory(c -> {
+        if(c.equals(MoveAttackController.class)){
+          return new MoveAttackController(window,player);
+        }
+        try{
+          return c.getConstructor().newInstance();
+        }catch(Exception e){
+          throw new RuntimeException(e);
+        }
+      });
+      Scene scene = new Scene(loaderStart.load());
+      window.setScene(scene);
+      window.show();
+  }catch(Exception e){
+    throw new RuntimeException(e);
+  }
+  }
+
+  @FXML
+  public void upgrade(){
+    
+  }
+
+  @FXML
+  public void done(){
+    
+  }
 }
---
+++ client/src/main/java/edu/duke/ece651/risc/client/PlayerInfoController.java
@@ -0,0 +1,15 @@
+package edu.duke.ece651.risc.client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class PlayerInfoController {
+  @FXML
+  protected Label playerValue;
+  @FXML
+  protected Label foodValue;
+  @FXML
+  protected Label techValue;
+  @FXML
+  protected Label maxValue;
+}
